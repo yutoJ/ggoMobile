2fd7386aff09a67c2ee516540cb9eaf63bcfe4b8
yutoJ <yutoogata4@gmail.com>
2017-10-10 01:35:35 +0900

    reservation view on calendar

--- src/components/CalendarModal/index.js
+++ src/components/CalendarModal/index.js
@@ -5,17 +5,21 @@ import {
   View,
   Text,
   ScrollView,
+  Image,
 } from 'react-native';
 
+//TODO
+import { MOCK_AVATAR2 } from '../../constants/secret';
 import moment from 'moment';
 
+const DATE_HEIGHT = 100;
 const styles = StyleSheet.create({
   container: {
     backgroundColor: 'white',
   },
   date: {
     flexDirection: 'row',
-    height: 100,
+    height: DATE_HEIGHT,
   },
   label: {
     fontSize: 20,
@@ -26,20 +30,76 @@ const styles = StyleSheet.create({
   },
   details: {
     flex: 1,
+    position: 'relative',
+    justifyContent: 'center',
   },
+  reservation: {
+    position: 'absolute',
+    backgroundColor: '#03A598',
+    padding: 20,
+    borderRadius: 5,
+    top: 0,
+    left: 0,
+    right: 10,
+    flexDirection: 'row',
+  },
+  info: {
+    flex: 1,
+  },
+  name: {
+    fontSize: 16,
+    color: 'white',
+    fontWeight: 'bold',
+  },
+  avatar: {
+    width: 60,
+    height: 60,
+    borderRadius: 60,
+  }
 });
 
 class CalendarModal extends Component {
 
   render() {
     const dates = moment.range(moment(), moment().add(1, 'months')).toArray('days');
+
+    const reservations = {
+      "2017-10-14": {
+        guest: {
+          name: "Jonny",
+          avatar: MOCK_AVATAR2
+        },
+        price: 500,
+        startDate: "2017-10-16",
+        endDate: "2017-10-18"
+      }
+    }
+
     return (
       <ScrollView style = {styles.container}>
         {dates.map((date, idx) => {
+
+          const reservation =reservations[date.format('YYYY-MM-DD')];
+          let reservationDuration = 0;
+          if (reservation) {
+              reservationDuration = moment.range(reservation.startDate, reservation.endDate).toArray('days').length;
+          }
+
           return (
             <View style = {styles.date} key = {idx}>
               <Text style = {styles.label}>{date.format('DD MMM')}</Text>
-              <View style = {styles.details}></View>
+              <View style = {styles.details}>
+                {
+                  reservation &&
+                  <View style = {[styles.reservation, { height: DATE_HEIGHT * reservationDuration }]}>
+                    <View style = { styles.info }>
+                      <Text style = { styles.name }>{reservation.guest.name}</Text>
+                      <Text style = {{ color: 'white' }}>{reservation.price}円</Text>
+                    </View>
+                    <Image style = { styles.avatar } source = {{ uri: reservation.guest.avatar }} />
+                  </View>
+                }
+              </View>
             </View>
           );
         })}
